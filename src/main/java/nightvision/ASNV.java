0569d656ed12c670b955d48eb53c4d84313bf0e3
AeternumStudios <aeternumstudios@gmail.com>
2018-03-11 09:56:14 -0700

    Setup

--- src/main/java/nightvision/ASNV.java
+++ src/main/java/nightvision/ASNV.java
@@ -1,6 +1,22 @@
 package nightvision;
 
-public class ASNV
-{
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class ASNV extends JavaPlugin implements Listener
+{
+	@Override
+	public void onDisable()
+	{
+
+	}
+
+	@Override
+	public void onEnable()
+	{
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void
 }
